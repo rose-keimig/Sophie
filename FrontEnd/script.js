78c982f1c209837e1de92fd6d7e89513b2590910
Rose Keimig <rose.keimig@gmail.com>
2025-06-13 15:56:30 -0500

    starting modal

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -133,4 +133,31 @@ loginForm.addEventListener("submit", async (event) => {
         errorMessage.textContent = "Login failed. Please try again.";
         errorMessage.style.display = "block";
     }
+
+ //Modal functionality - needs to be adjusted for new HTML
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
+
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  };
+  openModalBtn.addEventListener("click", openModal);
+
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  };
+
+  closeModalBtn.addEventListener("click", closeModal);
+  overlay.addEventListener("click", closeModal);
+  document.addEventListener("keydown");
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+      modalClose();
+    }
+  });
 });
